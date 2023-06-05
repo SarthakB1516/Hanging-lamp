0232f4e33de8d1b0cb9377f73706ccd1aaee1ea5
SarthakB1516 <115924989+SarthakB1516@users.noreply.github.com>
2023-06-05 13:21:09 +0530

    Lamp and body effect
    Lamp with on and off effect coordinating with the body.

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeColor()
+{
+    const body = document.querySelector('#body');
+    body.classList.toggle('bodyOn');
+}
+
+function bulbSwitchOn()
+{
+    const bulb = document.querySelector('.bulb');
+    bulb.classList.toggle('bulbOn');
+}
